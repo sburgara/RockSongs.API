94640eabeaa8167b8442cc4135bc88f92060d32a
Salvador Burgara <sburgara@gmail.com>
2025-04-04 17:38:23 -0700

    Application Running

--- RockSongs.API/Controllers/RockSongsController.cs
+++ RockSongs.API/Controllers/RockSongsController.cs
@@ -29,8 +29,8 @@ namespace RockSongs.API.Controllers
             _logger.LogCritical("Critical");
             RockSong rockSong = _rockSongsList.RandomSong();
             _logger.LogInformation(JsonConvert.SerializeObject(rockSong));
-            throw new Exception("Breaking the service");
-            //return Ok(rockSong);
+            //throw new Exception("Breaking the service");
+            return Ok(rockSong);
         }
     }
 }
